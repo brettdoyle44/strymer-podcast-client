18dcb4144590ddf501eaf6dd33509114cc832f43
brettdoyle44 <brettdoyle44@gmail.com>
2019-07-23 17:15:54 -0400

    Create index on signIn
    Built index that shows all podcasts in the DB.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,4 +7,7 @@ $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#sign-out').on('click', authEvents.onSignOut)
+
+  // podcast events
+  $('#sign-out').on('click', authEvents.onSignOut)
 })
--- assets/scripts/podcast/ui.js
+++ assets/scripts/podcast/ui.js
@@ -3,12 +3,34 @@
 const store = require('../store')
 
 const showPodcastsSuccess = responseData => {
-  $('#message').text('Eventually this will work.')
-  $('#message').removeClass('alert-danger')
-  $('#message').addClass('alert-success')
-  console.log(responseData)
-  store.user = responseData.user
+  store.podcasts = responseData.podcasts
+  $('.podcast-index').html('')
+  store.podcasts.forEach(function (podcast) {
+    const podcastsHtml = (`
+      <div class="col-4 mt-5">
+      <div class="card" style="width: 18rem;">
+        <img src="${podcast.image}" class="card-img-top card-click" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${podcast.title}</h5>
+          <p class="card-text">${podcast.publisher}</p>
+        </div>
+      </div>
+      </div>
+    `)
+
+    $('.podcast-index').append(podcastsHtml)
+  })
 }
+
+
+
+// const showPodcastsSuccess = responseData => {
+//   $('#message').text('Eventually this will work.')
+//   $('#message').removeClass('alert-danger')
+//   $('#message').addClass('alert-success')
+//   console.log(responseData)
+//   store.user = responseData.user
+// }
 
 const showPodcastsFail = () => {
   $('#message').text('Something went wrong')
